a7843174914fa633028ba8f535620a828c265187
Matvei <matvei@MacBook-Pro-Matvei.local>
2024-02-02 16:41:17 +0300

    123

--- js/main.js
+++ js/main.js
@@ -21,9 +21,6 @@ function setPreviewHeightInPixel() {
 
 setPreviewHeightInPixel(); 
 
-
-window.addEventListener('resize', setPreviewHeightInPixel);
-
 const removeSlidesForMobile = (slidesArray, slidesList, needSlidesCount) => {
   const offersForMobile = Array.from(slidesArray).slice(0, needSlidesCount);
   slidesList.innerHTML = '';
